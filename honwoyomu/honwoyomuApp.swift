3e07021da35a41286d5c7dcaec06d40573ee57df
gaowanqiu <gaowanqiu@bytedance.com>
2023-01-30 17:52:49 +0800

    Initial Commit

---
+++ honwoyomu/honwoyomuApp.swift
@@ -0,0 +1,17 @@
+//
+//  honwoyomuApp.swift
+//  honwoyomu
+//
+//  Created by gaoge on 2023/1/30.
+//
+
+import SwiftUI
+
+@main
+struct honwoyomuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
